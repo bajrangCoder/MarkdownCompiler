6fadc5e0471548200f3ca2f8a409ac489a3719ce
Raunak Raj <nkray21111983@gmail.com>
2023-05-06 11:19:02 +0530

    Update v1.0.1
    Added custom previewer for .md files in editor

--- readme.md
+++ readme.md
@@ -1,6 +1,25 @@
 # Markdown Styler
 
-The Markdown Styler plugin for Acode is designed to enhance the readability and design of Markdown of plugins,etc in the Acode editor. This plugin provides various styling options to make your Markdown content more visually appealing and easy to understand. 
+The Markdown Styler plugin for Acode is designed to enhance the readability and design of Markdown of plugins and It will also enhances markdown rendring in editor in the Acode editor. This plugin provides various styling options to make your Markdown content more visually appealing and easy to understand. 
+
+<details>
+    <summary>Updates</summary>
+    <br>
+    <details>
+        <summary>
+            <code><strong>v1.0.1</strong></code>
+        </summary>
+        <ul>
+            <li>Added markdown previewer for <code>.md</code> files</li>
+        </ul>
+    </details>
+</details>
+<br>
+
+Features
+----------
+- Add a markdown previewer for `.md` files in editor
+- Enhances markdown styling of plugin description 
 
 This plugin try to bring github markdown styling to Acode.
 
--- src/main.js
+++ src/main.js
@@ -3,15 +3,72 @@ import style from "./style.scss";
 
 class MarkdownStyler {
 
-    async init() {
+    async init($page) {
+        $page.id = "acode-plugin-markdown";
+        this.$markdowCompilerScript = tag("script",{
+            src: this.baseUrl+"markdown-it.min.js"
+        });
         this.$style = tag("style", {
             textContent: style
         });
-        document.head.append(this.$style);
+        this.$runBtn = tag('span', {
+          className: 'icon image',
+          attr: {
+            action: 'run',
+          },
+          onclick: this.run.bind(this),
+        });
+        this.$page = $page;
+        this.$page.settitle("Markdown Preview");
+        this.$mainDiv = tag("div",{
+            className:"mainDiv"
+        });
+        $page.append(this.$mainDiv);
+        const onhide = $page.onhide;
+        $page.onhide = () => {
+            this.$mainDiv.innerHTML = "";
+            onhide();
+        };
+        this.checkRunnable();
+        editorManager.on('switch-file', this.checkRunnable.bind(this));
+        editorManager.on('rename-file', this.checkRunnable.bind(this));
+        document.head.append(this.$markdowCompilerScript,this.$style);
+    }
+    
+    checkRunnable(){
+        const file = editorManager.activeFile;
+        if (this.$runBtn.isConnected) {
+          this.$runBtn.remove();
+        }
+        if (file?.name.endsWith('.md')) {
+          const $header = root.get('header');
+          $header.get('.icon.image')?.remove();
+          $header.insertBefore(this.$runBtn, $header.lastChild);
+        }
+    }
+    
+    run(){
+        this.$mdIt = window.markdownit({
+            html: true,
+            xhtmlOut: false,
+            breaks: false,
+            linkify: false,
+            typographer: true,
+            quotes: '“”‘’'
+        });
+        this.$mainDiv.innerHTML = this.$mdIt.render(editorManager.editor.getValue() || "");
+        this.$page.show();
     }
 
     async destroy() {
-
+        if (this.$runBtn) {
+          this.$runBtn.onclick = null;
+          this.$runBtn.remove();
+        }
+        editorManager.off('switch-file', this.checkRunnable.bind(this));
+        editorManager.off('rename-file', this.checkRunnable.bind(this));
+        this.$markdowCompilerScript.remove();
+        this.$style.remove();
     }
 }
 
